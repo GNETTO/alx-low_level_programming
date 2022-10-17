adbaf0a0317ae36e8495af19ecef8ab38d9ed2af
GNETTO <monsieur.gneto@gmail.com>
2022-10-17 16:07:36 -0700

    task2

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - comparerandom value
+ * Description: it is super
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is greater than 5\n", n);
+	elsee if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is less than 5\n", n);
+	return (0);
+
+}
