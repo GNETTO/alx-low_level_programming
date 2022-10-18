f4b32464fa165c9781356dbf1ca79c857d0d920e
GNETTO <monsieur.gneto@gmail.com>
2022-10-17 20:17:43 -0700

    task10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - combinison of number
+ * Description: above
+ * Return: return always 0
+ */
+
+in main(void)
+{
+	int row, col;
+
+	for (row = 48; row <= 57; row++)
+	{
+		for (col = 48; col <= 57; col++)
+		{
+			putchar(row);
+			putchar(col);
+			if (row != 57 || col != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
