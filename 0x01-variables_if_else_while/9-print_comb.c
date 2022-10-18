10af8bef608a74fb5be29750326c5cd52defad89
GNETTO <monsieur.gneto@gmail.com>
2022-10-17 19:40:29 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - search
+ * Description: all number combinaison here
+ * Return: return always 0 (success)
+ */
+
+int main(void)
+{
+	int row, col;
+
+	for (row = 48; row <= 57; row++)
+	{
+		for (col = 48; col <= 57; col++)
+		{
+			putchar(row);
+			putchar(col);
+			if (row == 57 || col == 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+}
