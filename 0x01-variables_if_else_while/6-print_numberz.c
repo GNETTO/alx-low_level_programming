2e80ead6bfac7964dca31da37110e142c76779d9
GNETTO <monsieur.gneto@gmail.com>
2022-10-17 18:24:00 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - decimal
+ * Description: description here
+ * Return: return 0 (success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 58; ch++)
+		printchar(ch);
+	printchar('\n');
+	return (0);
+}
