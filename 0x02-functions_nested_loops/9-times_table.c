0e8d9581db5be0b9c5dad4d2e24f3c60c552846a
GNETTO <monsieur.gneto@gmail.com>
2022-10-19 11:07:40 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,41 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - some explain
+ * times_table - some explain
  * Description: some for
  * Return: return 0 always
  */
 
-int main(void)
+void times_table(void)
 {
-	return  (0);
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			if (j == 0)
+			{
+				_putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (if k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
+	_putchar('\n');
 }
