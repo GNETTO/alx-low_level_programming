4d7fa7feaf9b98f20d71d214262456e912738a36
GNETTO <monsieur.gneto@gmail.com>
2022-10-26 01:16:58 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - make maultiplication of two numberss
+ * @a : int
+ * @b : int
+ * Return : return always a *b 
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
