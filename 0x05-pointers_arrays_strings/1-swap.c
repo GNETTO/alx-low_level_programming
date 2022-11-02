e86c3a72fa3c6ff13f8e50129a3071e768607c8d
GNETTO <monsieur.gneto@gmail.com>
2022-11-01 17:06:35 -0700

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swap 2 variables
+ * @a : int pointer
+ * @b : int pointer
+ * Return : return void
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
